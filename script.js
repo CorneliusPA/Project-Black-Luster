e44f5e74ed92a8338f4b1065a02095745c4c439a
Cornelius Allison <75234559+CorneliusPA@users.noreply.github.com>
2021-05-19 21:11:17 -0700

    Alpha 1 5/19/2021
    Buttons fully functional.
    Grid Layout Complete.
    
    Next Objective: Box in element into the grid.

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const downKeys = new Set();
         let x = 0;
         let y = 0;
-        let a = 0;
-        let b = 0;
-        let c = 0;
+        
         const p1 = document.getElementById('p1');
         const downKeysControl = document.getElementById('base');
         document.addEventListener('keydown', event => downKeys.add   (event.key));
@@ -13,7 +11,8 @@ const downKeys = new Set();
             downKeysControl.textContent = downKeys;
             const core = p1.style;
             function isDown(key) {return downKeys.has(key);}
-            const dir = isDown('Shift') ? 10 : 2;
+            const dir = isDown('Shift') ? 30 : 10;
+
             if (isDown('ArrowRight')) core.left = (x += dir) + 'px';
             if (isDown('ArrowDown' )) core.top  = (y += dir) + 'px';
             if (isDown('ArrowLeft' )) core.left = (x -= dir) + 'px';
@@ -22,12 +21,72 @@ const downKeys = new Set();
             if (isDown('s' )) core.top  = (y += dir) + 'px';
             if (isDown('a' )) core.left = (x -= dir) + 'px';
             if (isDown('w'   )) core.top  = (y -= dir) + 'px';
-            if (isDown('1' )) core.width  = (a += dir) + 'px', core.height = (b += dir) + 'px';
-            if (isDown('0' )) core.width  = (a -= dir) + 'px', core.height = (b -= dir) + 'px';
-            if (isDown('p' )) core.transform = 'rotate(' + (c += dir) + 'deg)';
-            if (isDown('o' )) core.transform = 'rotate(' + (c -= dir) + 'deg)';
-            
+    
             window.requestAnimationFrame(motion);
         }
+            motion();
+        
+        document.getElementById("Up").addEventListener("click", motionBtnUp)
+        document.getElementById("Right").addEventListener("click", motionBtnRight)
+        document.getElementById("Down").addEventListener("click", motionBtnDown)
+        document.getElementById("Left").addEventListener("click", motionBtnLeft)
+        document.getElementById("UR").addEventListener("click", motionBtnUR)
+        document.getElementById("UL").addEventListener("click", motionBtnUL)
+        document.getElementById("DR").addEventListener("click", motionBtnDR)
+        document.getElementById("DL").addEventListener("click", motionBtnDL)
+        //Up
+        function motionBtnUp(){
+            const core = p1.style;
+            const dir = 102;
+            core.top  = (y -= dir) + 'px';
+        }
+        //Right
+        function motionBtnRight(){
+            const core = p1.style;
+            const dir = 102;
+            core.left = (x += dir) + 'px';
+        }
+        //Down
+        function motionBtnDown(){
+            const core = p1.style;
+            const dir = 102;
+            core.top  = (y += dir) + 'px';
+        }
+        //Left
+        function motionBtnLeft(){
+            const core = p1.style;
+            const dir = 102;
+            core.left = (x -= dir) + 'px';
+        }
+        //Up-Right
+        function motionBtnUR(){
+            const core = p1.style;
+            const dir = 102;
+            core.top  = (y -= dir) + 'px';
+            core.left = (x += dir) + 'px';
+        }
+        //Down-Right
+        function motionBtnDR(){
+            const core = p1.style;
+            const dir = 102;
+            core.left = (x += dir) + 'px';
+            core.top  = (y += dir) + 'px';
+        }
+        //Down-Left
+        function motionBtnDL(){
+            const core = p1.style;
+            const dir = 102;
+            core.top  = (y += dir) + 'px';
+            core.left = (x -= dir) + 'px';
+        }
+        //Up-Left
+        function motionBtnUL(){
+            const core = p1.style;
+            const dir = 102;
+            core.top  = (y -= dir) + 'px';
+            core.left = (x -= dir) + 'px';
+        }
 
-        motion();
+        
+
+       
